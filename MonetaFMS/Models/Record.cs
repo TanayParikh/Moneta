64592c4300d80ce733cf2120e26dd7602a79c44f
Tanay Parikh <me@tanayparikh.com>
2018-02-18 12:52:46 -0500

    Added base Record model

---
+++ MonetaFMS/Models/Record.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonetaFMS.Models
+{
+    public abstract class Record
+    {
+        public int Id { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Note { get; set; }
+
+        protected Record()
+        {
+            if (CreationDate.Year == 1)
+                CreationDate = DateTime.Now;
+        }
+    }
+}
