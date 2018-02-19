24cb5e1d5f3cb95648f8f6f27fddd737255161d7
Tanay Parikh <me@tanayparikh.com>
2018-02-19 10:49:40 -0500

    Added default PDFService implementation

---
+++ MonetaFMS/Services/PDFService.cs
@@ -0,0 +1,30 @@
+﻿using MonetaFMS.Interfaces;
+using MonetaFMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonetaFMS.Services
+{
+    class PDFService : IPDFService
+    {
+        IConfigurationService ConfigurationService { get; set; }
+
+        public PDFService(IConfigurationService configurationService)
+        {
+            ConfigurationService = configurationService;
+        }
+
+        public bool GenerateInvoicePDF(Invoice invoice)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GenerateProfitLossPDF(Invoice invoice)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
