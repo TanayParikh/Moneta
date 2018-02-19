1c1276f6e9a8f7bfcfb8fd8a803aedc4232bf88c
Tanay Parikh <me@tanayparikh.com>
2018-02-18 20:12:00 -0500

    Added ExpenseCategory Enum

---
+++ MonetaFMS/Models/ExpenseCategory.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonetaFMS.Models
+{
+    public enum ExpenseCategory
+    {
+        MISC,
+        Advertising,
+        Insurance,
+        Banking,
+        OfficeExpenses,
+        OfficeMaintenance,
+        Legal,
+        Accounting,
+        Management,
+        Maintenance,
+        Salaries,
+        Wages,
+        Benefits,
+        PropertyTaxes,
+        Travel,
+        Utilities,
+        COGS,
+        Vehicle,
+        Lodging,
+        Parking,
+        UnionDues,
+        Telephone,
+        Internet
+    }
+
+    /* 
+        public static readonly string Advertising = "Advertising";
+        public static readonly string Insurance = "Insurance";
+        public static readonly string InterestAndBank = "Interest/bank charges";
+        public static readonly string OfficeExpenses = "Office expenses";
+        public static readonly string OfficeMaintenance = "Office maintenance";
+        public static readonly string Legal = "Legal fees and related expenses";
+        public static readonly string Accounting = "Accounting and other professional fees";
+        public static readonly string Management = "Management and admin fees";
+        public static readonly string Maintenance = "Maintenance and repair";
+        public static readonly string Salaries = "Salaries";
+        public static readonly string Wages = "Wages";
+        public static readonly string Benefits = "Benefits";
+        public static readonly string PropertyTaxes = "Property taxes";
+        public static readonly string Travel = "Travel";
+        public static readonly string Utilities = "Utilities";
+        public static readonly string COGS = "Cost of goods sold";
+        public static readonly string Vehicle = "Motor vehicle expenses";
+        public static readonly string Lodging = "Lodging";
+        public static readonly string Parking = "Parking fees";
+        public static readonly string MISC = "Other misc. supplies";
+        public static readonly string UnionDues = "Union professional and other similar dues";
+        public static readonly string Telephone = "Telephone";
+        public static readonly string Internet = "Internet and Communication";
+   */
+}
