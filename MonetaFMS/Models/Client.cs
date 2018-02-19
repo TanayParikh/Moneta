147d5d7eef812eb6c64689901921ab50a0c88269
Tanay Parikh <me@tanayparikh.com>
2018-02-18 20:12:27 -0500

    Added Client Model

---
+++ MonetaFMS/Models/Client.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace MonetaFMS.Models
+{
+    public class Client : Record
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => FirstName + " " + LastName;
+        public string Company { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+
+        public Client(int id, DateTime creation, string note, string firstName, string lastName,
+            string company, string address, string phoneNumber, string email) : this(note, firstName, lastName, company, address, phoneNumber, email)
+        {
+            Id = id;
+            CreationDate = creation;
+        }
+
+        public Client(string note, string firstName, string lastName,
+            string company, string address, string phoneNumber, string email) : base()
+        {
+            Note = note;
+            FirstName = firstName;
+            LastName = lastName;
+            Company = company;
+            Address = address;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+    }
+}
