7ba65e4dbaf4b6512f4c679bf869723c39ce1b03
Tanay Parikh <me@tanayparikh.com>
2018-02-19 10:48:43 -0500

    Added MainPage (Navigation View)

---
+++ MonetaFMS/MainPage.xaml.cs
@@ -0,0 +1,101 @@
+﻿using MonetaFMS.Pages;
+using MonetaFMS.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace MonetaFMS
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPageViewModel ViewModel { get; set; } = new MainPageViewModel();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            DataContext = ViewModel;
+        }
+        
+        /// <summary>
+        /// Sets default menu item to dashboard
+        /// </summary>
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
+        {
+            // set the initial SelectedItem 
+            foreach (NavigationViewItemBase item in NavView.MenuItems)
+            {
+                if (item is NavigationViewItem && item.Tag.ToString() == "dashboard")
+                {
+                    NavView.SelectedItem = item;
+                    break;
+                }
+            }
+
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            Window.Current.CoreWindow.SizeChanged += (s, ev) => UpdateAppTitle();
+            coreTitleBar.LayoutMetricsChanged += (s, ev) => UpdateAppTitle();
+        }
+
+        void UpdateAppTitle()
+        {
+            var full = (ApplicationView.GetForCurrentView().IsFullScreenMode);
+            var left = 12 + (full ? 0 : CoreApplication.GetCurrentView().TitleBar.SystemOverlayLeftInset);
+            AppTitle.Margin = new Thickness(left, 8, 0, 0);
+        }
+
+        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected)
+            {
+                ContentFrame.Navigate(typeof(SettingsPage));
+                ViewModel.PageTitle = "Settings";
+            }
+            else
+            {
+                NavigationViewItem item = args.SelectedItem as NavigationViewItem;
+                NavView_Navigate(item);
+                ViewModel.PageTitle = item.Content.ToString();
+            }
+        }
+
+        private void NavView_Navigate(NavigationViewItem item)
+        {
+            switch (item.Tag)
+            {
+                case "dashboard":
+                    ContentFrame.Navigate(typeof(DashboardPage));
+                    break;
+
+                case "invoices":
+                    ContentFrame.Navigate(typeof(InvoicesPage));
+                    break;
+
+                case "clients":
+                    ContentFrame.Navigate(typeof(ClientsPage));
+                    break;
+
+                case "expenses":
+                    ContentFrame.Navigate(typeof(ExpensesPage));
+                    break;
+            }
+
+            ViewModel.PageTitle = item.Content.ToString();
+        }
+    }
+}
