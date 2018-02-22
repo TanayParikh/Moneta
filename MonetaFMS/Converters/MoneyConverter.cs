d79352e75f8fa67d449cbbe045750c50d1a62d45
Tanay Parikh <me@tanayparikh.com>
2018-02-21 22:20:48 -0500

    Added MoneyConverter

---
+++ MonetaFMS/Converters/MoneyConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace MonetaFMS.Converters
+{
+    public class MoneyConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is decimal i)
+            {
+                if (i < 100_000)
+                {
+                    return ((int)i).ToString("C0", CultureInfo.CurrentCulture).Replace(',', ' ');
+                }
+                else
+                {
+                    return ((int)i / 1000).ToString("C0", CultureInfo.CurrentCulture).Replace(',', ' ') + "K";
+                }
+            }
+
+            throw new InvalidOperationException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
