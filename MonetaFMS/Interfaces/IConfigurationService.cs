ee871f096552c195daa2790dd9b462f240bc555f
Tanay Parikh <me@tanayparikh.com>
2018-02-22 22:03:31 -0500

    Added configuration service

---
+++ MonetaFMS/Interfaces/IConfigurationService.cs
@@ -0,0 +1,18 @@
+﻿using MonetaFMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonetaFMS.Interfaces
+{
+    public interface IConfigurationService
+    {
+        BusinessProfile BusinessProfile { get; }
+        MonetaSettings MonetaSettings { get; }
+
+        bool UpdateBusinessProfile(BusinessProfile businessProfile);
+        bool UpdateMonetaSettings(MonetaSettings monetaSettings);
+    }
+}
