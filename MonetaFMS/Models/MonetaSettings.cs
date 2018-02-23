c5de770a1d2ecaa5966db86cf3cfd1c1b39f2388
Tanay Parikh <me@tanayparikh.com>
2018-02-22 20:27:50 -0500

    Added settings model (bindable / serializable)

---
+++ MonetaFMS/Models/MonetaSettings.cs
@@ -0,0 +1,30 @@
+﻿using MonetaFMS.Common;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonetaFMS.Models
+{
+    public class MonetaSettings : BindableBase
+    {
+        bool _openPDFOnCreation;
+
+        [JsonProperty]
+        public bool OpenPDFOnCreation
+        {
+            get { return _openPDFOnCreation; }
+            set { SetProperty(ref _openPDFOnCreation, value); }
+        }
+
+        public MonetaSettings(bool openPDFOnCreation = true)
+        {
+            OpenPDFOnCreation = openPDFOnCreation;
+        }
+
+        // JsonConvert Default Constructor
+        public MonetaSettings() { }
+    }
+}
