90f4f107afed296b483a5c4426c740cb77343932
Tanay Parikh <me@tanayparikh.com>
2018-02-24 14:57:29 -0500

    VM for main page

---
+++ MonetaFMS/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using MonetaFMS.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonetaFMS.ViewModels
+{
+    public class MainPageViewModel : BindableBase
+    {
+        private string pageTitle = "Moneta FMS";
+        private bool isPaneOpen = true;
+
+        public MainPageViewModel()
+        {
+        }
+
+        public string PageTitle
+        {
+            get { return pageTitle; }
+            set { SetProperty(ref pageTitle, value); }
+        }
+
+        public bool IsPaneOpen
+        {
+            get { return isPaneOpen; }
+            set { SetProperty(ref isPaneOpen, value); }
+        }
+    }
+}
