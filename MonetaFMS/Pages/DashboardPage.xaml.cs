57972c96087d46222c6382c60616d4c36edcf47e
Tanay Parikh <me@tanayparikh.com>
2018-02-24 17:24:20 -0500

    Dashboard page utilizing Dashboard.html + WebView.InvokeScriptAsync

---
+++ MonetaFMS/Pages/DashboardPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using MonetaFMS.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace MonetaFMS.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DashboardPage : Page
+    {
+        public DashboardPageViewModel ViewModel { get; set; } = new DashboardPageViewModel();
+
+        public DashboardPage()
+        {
+            this.InitializeComponent();
+            DataContext = ViewModel;
+            
+            string topClients = ViewModel.GetTopClients();
+            string pastPerformance = ViewModel.GetPastPerformance();
+            string topExpenseCategories = ViewModel.GetTopExpenseCategories();
+
+            Loaded += (s, e) =>
+            {
+                webviews.NavigationCompleted += (se, ev) => SetValues(topClients, pastPerformance, topExpenseCategories);
+                webviews.Navigate(new Uri("ms-appx-web:///Resources/Dashboard.html"));
+            };
+
+            // Necessary due to improper memory management by UWP Webview
+            Unloaded += (s, e) => GC.Collect();
+        }
+
+        private async void SetValues(string topClients, string pastPerformance, string topExpenseCategories)
+        {
+            var topClientsResult = await webviews.InvokeScriptAsync("setupTopClients", new string[] { topClients });
+            var pastPerformanceResult = await webviews.InvokeScriptAsync("setupRevenueExpense", new string[] { pastPerformance });
+            var expenseCategoriesResult = await webviews.InvokeScriptAsync("setupTopExpenseCategories", new string[] { topExpenseCategories });
+        }
+    }
+}
---
+++ MonetaFMS/ViewModels/DashboardPageViewModel.cs
@@ -0,0 +1,131 @@
+﻿using MonetaFMS.Common;
+using MonetaFMS.Interfaces;
+using MonetaFMS.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonetaFMS.ViewModels
+{
+    public class DashboardPageViewModel : BindableBase
+    {
+        public class GraphData
+        {
+            public string[] labels;
+            public Datasets[] datasets;
+            
+            public class Datasets
+            {
+                public int[] data;
+                public string[] backgroundColor;
+                public string label;
+            }
+        }
+
+        IClientService ClientService { get; set; }
+        IInvoiceService InvoiceService { get; set; }
+        IBusinessStatsService BusinessService { get; set; }
+
+        private string pieChartHTML;
+        
+        public string PieChartHTML
+        {
+            get { return pieChartHTML; }
+            set { SetProperty(ref pieChartHTML, value); }
+        }
+        
+        string[] backgroundColours =
+                    {
+                            "rgb(255, 99, 132)", // red
+                            "rgb(255, 159, 64)", // orange
+                            "rgb(255, 205, 86)", // yellow
+                            "rgb(75, 192, 192)", // green
+                            "rgb(54, 162, 235)", // blue
+                            "rgb(153, 102, 255)", // purple
+                            "rgb(201, 203, 207)" // grey
+                        };
+
+        public DashboardPageViewModel()
+        {
+            ClientService = Services.Services.ClientService;
+            InvoiceService = Services.Services.InvoiceService;
+            BusinessService = Services.Services.BusinessStatsService;
+            //GetDashboardStats();
+        }
+
+        public string GetTopClients()
+        {
+            Dictionary<Client, Decimal> topClients = BusinessService.GetTopClients(5);
+
+            var graphData = new GraphData
+            {
+                labels = topClients.Select(c => c.Key.Company).ToArray(),
+                datasets = new GraphData.Datasets[]
+                {
+                    new GraphData.Datasets
+                    {
+                        data = topClients.Select(c => (int)c.Value).ToArray(),
+                        backgroundColor = backgroundColours
+                    }
+                }
+            };
+
+            return JsonConvert.SerializeObject(graphData);
+        }
+
+        public string GetTopExpenseCategories()
+        {
+            Dictionary<ExpenseCategory, Decimal> topExpenses = BusinessService.GetTopExpenseCategories(5);
+
+            var graphData = new GraphData
+            {
+                labels = topExpenses.Select(c => c.Key.ToString()).ToArray(),
+                datasets = new GraphData.Datasets[]
+                {
+                    new GraphData.Datasets
+                    {
+                        data = topExpenses.Select(c => (int)c.Value).ToArray(),
+                        backgroundColor = backgroundColours
+                    }
+                }
+            };
+
+            return JsonConvert.SerializeObject(graphData);
+        }
+
+        public string GetPastPerformance()
+        {
+            List<(string month, decimal revenue, decimal expenses)> pastPerformance = Services.Services.BusinessStatsService.GetPerformance(6);
+
+            string[] backgroundColorGreen = { "rgba(151, 205, 118, .7)" };
+            string[] backgroundColorBlue = { "rgba(31, 200, 219, .7)" };
+
+            var graphData = new GraphData
+            {
+                labels = pastPerformance.Select(c => c.month).ToArray(),
+                datasets = new GraphData.Datasets[]
+                {
+                    new GraphData.Datasets
+                    {
+                        label = "Revenue",
+                        data = pastPerformance.Select(c => (int)c.revenue).ToArray(),
+                        backgroundColor = backgroundColorGreen
+                    },
+
+                    new GraphData.Datasets
+                    {
+                        label = "Expenses",
+                        data = pastPerformance.Select(c => (int)c.expenses).ToArray(),
+                        backgroundColor = backgroundColorBlue
+                    }
+                }
+            };
+
+            return JsonConvert.SerializeObject(graphData);
+        }
+
+    }
+}
