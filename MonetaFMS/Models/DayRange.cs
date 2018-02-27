01282de4148201a6fa8600a12745802759017821
Tanay Parikh <me@tanayparikh.com>
2018-02-26 23:04:40 -0500

    Added DayRange enum

---
+++ MonetaFMS/Models/DayRange.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonetaFMS.Models
+{
+    public enum DayRange
+    {
+        DueIn14,
+        DueIn29,
+        DueIn44,
+        DueIn89,
+        DueIn90Plus,
+        Overdue
+    }
+
+    public static class DayRangeExtensions
+    {
+        public static string ToFriendlyString(this DayRange range)
+        {
+            switch (range)
+            {
+                case DayRange.DueIn14:
+                    return "Next 14 Days";
+                case DayRange.DueIn29:
+                    return "15-29 Days";
+                case DayRange.DueIn44:
+                    return "30-44 Days";
+                case DayRange.DueIn89:
+                    return "45-89 Days";
+                case DayRange.DueIn90Plus:
+                    return "90+ Days";
+                case DayRange.Overdue:
+                    return "Overdue";
+                default:
+                    return range.ToString();
+            }
+        }
+    }
+}
