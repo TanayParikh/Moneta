96b81f18dbf43c19b3dbfa6e4127ffdb2b6cd6d4
Tanay Parikh <me@tanayparikh.com>
2018-03-11 18:51:09 -0400

    Date converter

---
+++ MonetaFMS/Converters/DateConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace MonetaFMS.Converters
+{
+    public class DateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is DateTime date && parameter is string param)
+            {
+                if (param == "MonthShort")
+                {
+                    return date.ToString("MMM");
+                }
+                else if (param == "DayOfMonth")
+                {
+                    return date.Day;
+                }
+
+                return date.ToString();
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
