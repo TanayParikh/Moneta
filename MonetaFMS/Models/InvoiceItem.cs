e2bae285ec593b971f7e25ac3169c7ad58acaebc
Tanay Parikh <me@tanayparikh.com>
2018-03-14 22:55:14 -0400

    Made description, price and taxPercentage bindable

--- MonetaFMS/Models/InvoiceItem.cs
+++ MonetaFMS/Models/InvoiceItem.cs
@@ -4,8 +4,19 @@ namespace MonetaFMS.Models
 {
     public class InvoiceItem : Record
     {
-        public string Description { get; set; }
-        public decimal Price { get; set; }
+        string _description;
+        public string Description
+        {
+            get { return _description; }
+            set { SetProperty(ref _description, value); }
+        }
+
+        decimal _price;
+        public decimal Price
+        {
+            get { return _price; }
+            set { SetProperty(ref _price, value); }
+        }
 
         private decimal _taxPercentage;
         public decimal TaxPercentage
@@ -16,12 +27,12 @@ namespace MonetaFMS.Models
             {
                 if (value < 0 || value > 1)
                     throw new ArgumentOutOfRangeException("Tax percentage must be between 0 and 1");
-
-                _taxPercentage = value;
+                
+                SetProperty(ref _taxPercentage, value);
             }
         }
-        public int InvoiceId { get; set; }
 
+        public int InvoiceId { get; set; }
 
         public InvoiceItem(int id, DateTime creation, string note, string description, decimal price, decimal taxPercentage, int invoiceId)
             : this(note, description, price, taxPercentage, invoiceId)
--- MonetaFMS/Models/Record.cs
+++ MonetaFMS/Models/Record.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MonetaFMS.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace MonetaFMS.Models
 {
-    public abstract class Record
+    public abstract class Record : BindableBase
     {
         public int Id { get; set; }
         public DateTime CreationDate { get; set; }
