1ef4a09e7b951c137b359914dd638c900113ea36
Tanay Parikh <me@tanayparikh.com>
2018-03-17 22:13:41 -0400

    Added InvoiceTypeExtensions ToString

--- MonetaFMS/Models/InvoiceType.cs
+++ MonetaFMS/Models/InvoiceType.cs
@@ -6,4 +6,20 @@
         Quote,
         SalesOrder
     }
+
+    public static class InvoiceTypeExtensions
+    {
+        public static string ToString(this InvoiceType invoiceType)
+        {
+            switch (invoiceType)
+            {
+                case InvoiceType.SalesOrder:
+                    return "Sales Order";
+                case InvoiceType.Invoice:
+                case InvoiceType.Quote:
+                default:
+                    return invoiceType.ToString();
+            }
+        }
+    }
 }
