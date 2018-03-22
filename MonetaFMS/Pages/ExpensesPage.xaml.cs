417ffc01f0e400b4c25c8f34a8fac5559fea0fdf
Tanay Parikh <me@tanayparikh.com>
2018-03-21 22:03:18 -0400

    Expense Page List View with CRU functionality

--- MonetaFMS/Pages/ExpensesPage.xaml.cs
+++ MonetaFMS/Pages/ExpensesPage.xaml.cs
@@ -1,30 +1,168 @@
-﻿using System;
+﻿using MonetaFMS.Common;
+using MonetaFMS.Converters;
+using MonetaFMS.Models;
+using MonetaFMS.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace MonetaFMS.Pages
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class ExpensesPage : Page
     {
+        public ExpensesPageViewModel ViewModel { get; set; } = new ExpensesPageViewModel();
+        private (Grid preview, Grid detail) PreviousItem { get; set; }
+        private MoneyConverter MoneyConverter { get; } = new MoneyConverter();
+        
         public ExpensesPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            DataContext = ViewModel;
+        }
+
+        private void ListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            EditExpense(e.ClickedItem as Expense);
+        }
+
+        private void EditExpense(Expense expense)
+        {
+            if (ExpensesList.ContainerFromItem(expense) is ListViewItem container)
+            {
+                ClosePreviousItem();
+
+                (Grid preview, Grid detail) selectedItem = 
+                    (
+                        Child<Grid>(container, "ExpensePreview"),
+                        Child<Grid>(container, "ExpenseDetail")
+                    );
+
+                selectedItem.preview.Visibility = Visibility.Collapsed;
+                selectedItem.detail.Visibility = Visibility.Visible;
+
+                PreviousItem = selectedItem;
+                ViewModel.Expense = container.Content as Expense;
+            }
+        }
+
+        private void ClosePreviousItem()
+        {
+            if (PreviousItem.preview != null)
+                PreviousItem.preview.Visibility = Visibility.Visible;
+            if (PreviousItem.detail != null)
+                PreviousItem.detail.Visibility = Visibility.Collapsed;
+        }
+
+        public T Child<T>(DependencyObject parent, string name) where T : FrameworkElement
+        {
+            return Children(parent).OfType<T>().FirstOrDefault(x => x.Name == name);
+        }
+
+        public List<DependencyObject> Children(DependencyObject parent)
+        {
+            var list = new List<DependencyObject>();
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+            {
+                if (VisualTreeHelper.GetChild(parent, i) is DependencyObject child)
+                {
+                    list.Add(child);
+                    list.AddRange(Children(child));
+                }
+            }
+            return list;
+        }
+
+        private void SearchBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            ViewModel.Search(sender.Text);
+        }
+
+        private async void CreateExpense_Click(object sender, RoutedEventArgs e)
+        {
+            var newExpense = ViewModel.CreateExpense();
+            ExpensesList.ScrollIntoView(newExpense);
+            await Task.Delay(100);
+            EditExpense(newExpense);
+        }
+
+        private void Cost_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            try
+            {
+                // Doesn't register key if key leads to invalid input
+                e.Handled = MoneyConverter.ConvertBack(((TextBox)sender).Text + Utilities.GetCharFromKey(e.Key)) == null;
+            }
+            catch (Exception)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.CancelEdit();
+            ClosePreviousItem();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.Save();
+            ClosePreviousItem();
+        }
+
+        private void AttachBox_DragLeave(object sender, DragEventArgs e)
+        {
+            Child<Grid>(PreviousItem.detail, "IdleState").Visibility = Visibility.Visible;
+            Child<Grid>(PreviousItem.detail, "ActiveState").Visibility = Visibility.Collapsed;
+        }
+
+        private void AttachBox_DragEnter(object sender, DragEventArgs e)
+        {
+            Child<Grid>(PreviousItem.detail, "IdleState").Visibility = Visibility.Collapsed;
+            Child<Grid>(PreviousItem.detail, "ActiveState").Visibility = Visibility.Visible;
+        }
+
+        private async void AttachBox_Drop(object sender, DragEventArgs e)
+        {
+            if (e.DataView.Contains(StandardDataFormats.StorageItems))
+            {
+                var items = await e.DataView.GetStorageItemsAsync();
+                if (items.Count > 0)
+                {
+                    await ViewModel.SetExpenseDocumentation(items[0] as StorageFile);
+                    //var bitmapImage = new BitmapImage();
+                    //bitmapImage.SetSource(await storageFile.OpenAsync(FileAccessMode.Read));
+                    // Set the image on the main page to the dropped image
+                    //Image.Source = bitmapImage;
+                }
+            }
+
+            AttachBox_DragLeave(null, null);
+        }
+
+        private void AttachBox_DragOver(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = DataPackageOperation.Copy;
+        }
+
+        private async void Document_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            await ViewModel.OpenDoc();
         }
     }
 }
---
+++ MonetaFMS/ViewModels/ExpensesPageViewModel.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Toolkit.Uwp.UI;
+using MonetaFMS.Common;
+using MonetaFMS.Interfaces;
+using MonetaFMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.UI.Xaml;
+
+namespace MonetaFMS.ViewModels
+{
+    public class ExpensesPageViewModel : BindableBase
+    {
+        private ObservableCollection<Expense> _allExpenses { get; set; }
+        public AdvancedCollectionView AllExpenses { get; set; }
+
+        public Expense ExpenseBackup { get; set; }
+
+        public List<ExpenseCategory> ExpenseCategories { get; } =
+            Enum.GetValues(typeof(ExpenseCategory)).Cast<ExpenseCategory>().ToList();
+
+        private Expense _Expense;
+        public Expense Expense
+        {
+            get { return _Expense; }
+            set
+            {
+                if (SetProperty(ref _Expense, value))
+                {
+                    ExpenseBackup = Extensions.Clone(_Expense);
+                }
+            }
+        }
+
+        public IExpenseService ExpenseService { get; set; }
+        
+        public ExpensesPageViewModel()
+        {
+            _allExpenses = new ObservableCollection<Expense>(Services.Services.ExpenseService.AllItems);
+            AllExpenses = new AdvancedCollectionView(_allExpenses);
+
+            // Default sorts to descending id
+            AllExpenses.SortDescriptions.Add(new SortDescription("Id", SortDirection.Descending));
+
+            ExpenseService = Services.Services.ExpenseService;
+        }
+
+        internal void Search(string text)
+        {
+
+        }
+
+        internal void CancelEdit()
+        {
+            if (Expense.Id == -1)
+            {
+                AllExpenses.Remove(Expense);
+            }
+            else
+            {
+                Expense.Category = ExpenseBackup.Category;
+                Expense.Description = ExpenseBackup.Description;
+                Expense.TotalCost = ExpenseBackup.TotalCost;
+                Expense.TaxComponent = ExpenseBackup.TaxComponent;
+                Expense.Date = ExpenseBackup.Date;
+                Expense.ImageReference = ExpenseBackup.ImageReference;
+                Expense.Invoice = ExpenseBackup.Invoice;
+            }
+        }
+
+        internal bool Save()
+        {
+            return Expense.Id == -1 ?
+                ExpenseService.CreateEntry(Expense).Id > 0 :
+                ExpenseService.UpdateEntry(Expense);
+        }
+
+        internal Expense CreateExpense()
+        {
+            var newExpense = ExpenseService.NewExpense();
+            AllExpenses.Add(newExpense);
+            return newExpense;
+        }
+
+        internal async Task SetExpenseDocumentation(StorageFile storageFile)
+        {
+            var receiptsFolder = await ApplicationData.Current.LocalFolder.CreateFolderAsync("Receipts", CreationCollisionOption.OpenIfExists);
+            var storedReceipt = await storageFile.CopyAsync(receiptsFolder, storageFile.Name, NameCollisionOption.GenerateUniqueName);
+
+            Expense.ImageReference = storedReceipt.Path;
+        }
+
+        internal async Task OpenDoc()
+        {
+            var file = await(await ApplicationData.Current.LocalFolder.GetFolderAsync("Receipts"))?.GetFileAsync(Expense.DocumentName);
+
+            if (file != null)
+            {
+                // Launch the retrieved file
+                var success = await Windows.System.Launcher.LaunchFileAsync(file);
+            }
+        }
+    }
+}
