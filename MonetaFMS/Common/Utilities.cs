99884c9df153d095df0895fcde165b477a5b9b50
Tanay Parikh <me@tanayparikh.com>
2018-03-23 21:19:42 -0400

    File name sanitizer utility

--- MonetaFMS/Common/Utilities.cs
+++ MonetaFMS/Common/Utilities.cs
@@ -64,5 +64,14 @@ namespace MonetaFMS.Common
             return ch;
         }
         #endregion
+
+        // https://stackoverflow.com/a/847251
+        public static string MakeValidFileName(string name)
+        {
+            string invalidChars = System.Text.RegularExpressions.Regex.Escape(new string(System.IO.Path.GetInvalidFileNameChars()));
+            string invalidRegStr = string.Format(@"([{0}]*\.+$)|([{0}]+)", invalidChars);
+
+            return System.Text.RegularExpressions.Regex.Replace(name, invalidRegStr, "_");
+        }
     }
 }
