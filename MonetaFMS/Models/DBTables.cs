cef485855ff96747cb01999220357afddd0b8003
Tanay Parikh <me@tanayparikh.com>
2018-04-02 21:06:17 -0400

    DBTables

---
+++ MonetaFMS/Models/DBTables.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonetaFMS.Models
+{
+    public enum DBTables
+    {
+        Invoices,
+        Clients,
+        Expenses,
+        Items,
+        Payments
+    }
+}
